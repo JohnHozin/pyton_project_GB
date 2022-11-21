fb1b52c75522deebfbb688ac8207bcc133f1c728
JohnHozin <simple_protector@mail.ru>
2022-11-21 22:17:11 +0300

    add Home Work

--- home_works/home_work_1/task_3.py
+++ home_works/home_work_1/task_3.py
@@ -3,8 +3,10 @@
 x = int(input('Введите координату х: '))
 y = int(input('Введите координату у: '))
 
+
 def print_xy(x, y, z):
     print(f'x = {x}; y = {y} -> {z}')
+
 
 if x > 0 and y > 0:
     print_xy(x, y, 1)
